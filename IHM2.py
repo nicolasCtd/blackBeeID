3dc06e69ef76d3a9abd732669f5fc9d6d7af52f2
nicolas <nicolas-coutand@hotmail.fr>
2025-01-06 23:11:54 +0100

    With this version of the code we can load 10 images (2 tabs) with the "Load" button and we can open a secondary window for the editing of these images.

--- IHM2.py
+++ IHM2.py
@@ -25,7 +25,22 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Second(QMainWindow):
     def __init__(self, parent=None):
         super(Second, self).__init__(parent)
+        self.setWindowTitle("Fenêtre d'édition")
 
+    def display(self, fileName):
+        layout = QVBoxLayout(self)
+        label = QLabel(self)
+        pixmap = QPixmap(fileName)
+        label.setPixmap(pixmap)
+        self.setCentralWidget(label) 
+        self.setLayout(layout)
+    
+    def display_error_message(self):
+        label = QLabel("Veuillez d'abord charger une image avant de l'éditer")
+        layout = QVBoxLayout()
+        self.setCentralWidget(label)
+        self.setLayout(layout)
+        
 
 class MainWindow(QMainWindow):
     
@@ -34,15 +49,27 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Analyse des ailes d'abeilles")
 
-        self.tab_widget = Tab(self) 
-        self.setCentralWidget(self.tab_widget) 
+        self.tab_widget = Tab(self)
+        self.setCentralWidget(self.tab_widget)
 
 
 class Tab(QWidget): 
     def __init__(self, parent): 
-        super(QWidget, self).__init__(parent) 
+        super(QWidget, self).__init__(parent)
 
-        self.layout = QVBoxLayout(self) 
+        self.fileName1 = "im1.png"
+        self.fileName2 = "im2.png"
+        self.fileName3 = "im3.png"
+        self.fileName4 = "im4.png"
+        self.fileName5 = "im5.png"
+        self.fileName6 = "im6.png"
+        self.fileName7 = "im7.png"
+        self.fileName8 = "im8.png"
+        self.fileName9 = "im9.png"
+        self.fileName10 = "im10.png"
+        self.fileName11 = "im11.png"
+
+        self.layout = QVBoxLayout(self)
   
         # Initialize tab screen 
         self.tabs = QTabWidget()
@@ -102,8 +129,8 @@ class Tab(QWidget):
         connections_load = {1:self.browseFile1, 2:self.browseFile2, 3:self.browseFile3, 4:self.browseFile4, 5:self.browseFile5,
                        6:self.browseFile6, 7:self.browseFile7, 8:self.browseFile8, 9:self.browseFile9, 10:self.browseFile10}
         
-        # connections_edit = {1:self.editFile1, 2:self.editFile2, 3:self.editFile3, 4:self.editFile4, 5:self.editFile5,
-        #                6:self.editFile6, 7:self.editFile7, 8:self.editFile8, 9:self.editFile9, 10:self.editFile10}
+        connections_edit = {1:self.editFile1, 2:self.editFile2, 3:self.editFile3, 4:self.editFile4, 5:self.editFile5,
+                       6:self.editFile6, 7:self.editFile7, 8:self.editFile8, 9:self.editFile9, 10:self.editFile10}
 
         self.grids = list()
 
@@ -137,9 +164,8 @@ class Tab(QWidget):
                 self.grids[-1].addWidget(btn2, i, 2)
 
                 btn1.clicked.connect(connections_load[num_image])
-                # btn2.clicked.connect(connections_edit[num_image])
-                btn2.clicked.connect(self.editFile)
-                
+                btn2.clicked.connect(connections_edit[num_image])
+                # btn2.clicked.connect(self.editFile)
 
         # Create first tab 
         self.tab1.layout = self.grids[0]
@@ -154,44 +180,102 @@ class Tab(QWidget):
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
         
-    def editFile(self):
-        self.dialog = Second()
+    def editFile1(self):
+        if self.fileName1 == "im1.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName1)
         self.dialog.show()
         
     def editFile2(self):
-        print("bbbbb")
-        self.dialog[1].show()
+        if self.fileName2 == "im2.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName2)
+        self.dialog.show()
     
     def editFile3(self):
-        self.dialog[2].show()
+        if self.fileName3 == "im3.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName3)
+        self.dialog.show()
     
     def editFile4(self):
-        self.dialog[3].show()
+        if self.fileName4 == "im4.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName4)
+        self.dialog.show()
 
     def editFile5(self):
-        self.dialog[4].show()
+        if self.fileName5 == "im5.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName5)
+        self.dialog.show()
     
     def editFile6(self):
-        self.dialog[5].show()
+        if self.fileName6 == "im6.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName6)
+        self.dialog.show()
     
     def editFile7(self):
-        self.dialog[6].show()
+        if self.fileName7 == "im7.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName7)
+        self.dialog.show()
     
     def editFile8(self):
-        self.dialog[7].show()
+        if self.fileName8 == "im8.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName8)
+        self.dialog.show()
     
     def editFile9(self):
-        self.dialog[8].show()
+        if self.fileName9 == "im9.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName8)
+        self.dialog.show()
     
     def editFile10(self):
-        self.dialog[9].show()
+        if self.fileName10 == "im10.png":
+            self.dialog = Second()
+            self.dialog.display_error_message()
+        else:
+            self.dialog = Second()
+            self.dialog.display(self.fileName10)
+        self.dialog.show()
         
     def browseFile1(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName1, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName1 != "":
+            pixmap = QPixmap(self.fileName1)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[0].setPixmap(pixmap)
             self.grids[0].addWidget(self.label_left[0], 0, 1)
@@ -202,9 +286,9 @@ class Tab(QWidget):
     def browseFile2(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName2, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName2 != "":
+            pixmap = QPixmap(self.fileName2)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[1].setPixmap(pixmap)
             self.grids[0].addWidget(self.label_left[1], 1, 1)
@@ -215,9 +299,9 @@ class Tab(QWidget):
     def browseFile3(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName3, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName3 != "":
+            pixmap = QPixmap(self.fileName3)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[2].setPixmap(pixmap)
             self.grids[0].addWidget(self.label_left[2], 2, 1)
@@ -228,9 +312,9 @@ class Tab(QWidget):
     def browseFile4(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName4, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName4 != "":
+            pixmap = QPixmap(self.fileName4)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[3].setPixmap(pixmap)
             self.grids[0].addWidget(self.label_left[3], 3, 1)
@@ -241,9 +325,9 @@ class Tab(QWidget):
     def browseFile5(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName5, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName5 != "":
+            pixmap = QPixmap(self.fileName5)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[4].setPixmap(pixmap)
             self.grids[0].addWidget(self.label_left[4], 4, 1)
@@ -254,9 +338,9 @@ class Tab(QWidget):
     def browseFile6(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName6, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName6 != "":
+            pixmap = QPixmap(self.fileName6)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[5].setPixmap(pixmap)
             self.grids[1].addWidget(self.label_left[5], 0, 1)
@@ -267,9 +351,9 @@ class Tab(QWidget):
     def browseFile7(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName7, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName7 != "":
+            pixmap = QPixmap(self.fileName7)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[6].setPixmap(pixmap)
             self.grids[1].addWidget(self.label_left[6], 1, 1)
@@ -280,9 +364,9 @@ class Tab(QWidget):
     def browseFile8(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName8, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName8 != "":
+            pixmap = QPixmap(self.fileName8)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[7].setPixmap(pixmap)
             self.grids[1].addWidget(self.label_left[7], 2, 1)
@@ -293,9 +377,9 @@ class Tab(QWidget):
     def browseFile9(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName9, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName9 != "":
+            pixmap = QPixmap(self.fileName9)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[8].setPixmap(pixmap)
             self.grids[1].addWidget(self.label_left[8], 3, 1)
@@ -306,9 +390,9 @@ class Tab(QWidget):
     def browseFile10(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
-        if fileName != "":
-            pixmap = QPixmap(fileName)
+        self.fileName10, _ = QFileDialog.getOpenFileName(self, "Select File", "", "All Files (*)", options=options)
+        if self.fileName10 != "":
+            pixmap = QPixmap(self.fileName10)
             pixmap = pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio, Qt.FastTransformation)
             self.label_left[9].setPixmap(pixmap)
             self.grids[1].addWidget(self.label_left[9], 4, 1)
